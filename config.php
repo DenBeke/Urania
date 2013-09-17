0e48cc93f4653c70cd1b9f2375f89caa450708c6
MathiasB <git@denbeke.be>
2013-09-17 15:26:01 +0200

    Config file added

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+//Connection details for the database
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = 'root';
+$db_database = 'Urania';
+
+$db_table_albums = 'Albums';
+$db_table_images = 'Images';
+
+
+//User login
+$user_name = 'root';
+$user_password = 'root';
+?>
