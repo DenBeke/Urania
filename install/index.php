35f4fa275d2c36728b4505972d9bccb857457357
MathiasB <git@denbeke.be>
2014-02-09 14:23:23 +0100

    Added base of installation setup

---
+++ install/index.php
@@ -0,0 +1,63 @@
+<?php 
+/*
+Installation Setup for Urania
+
+Author: Mathias Beke
+Url: http://denbeke.be
+Date: February 2014
+*/
+
+require_once(dirname(__FILE__).'/install.php');
+
+
+if(isset($_POST['db_host']) && isset($_POST['db_user']) && isset($_POST['db_password']) && isset($_POST['db_database'])) {
+
+	//Connect to database
+	$db_host = $_POST['db_host'];
+	$db_user = $_POST['db_user'];
+	$db_password = $_POST['db_password'];
+	$db_database = $_POST['db_database'];
+	
+	$link = connectDatabase($db_host, $db_user, $db_password, $db_database);
+	
+	//Add the tables
+	createAlbumsTable($link);
+	createImagesTable($link)
+	
+	//Create the 'upload' dir
+	mkdir(dirname(__FILE__).'/../upload');
+	
+	
+	//TODO
+	//Fetch username
+	//Fetch the site url
+	//Write everything to the config file
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Install Urania</title>
+	
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+	<form method="post" action="index.php">
+	
+		<input type="text" name="db_host" value="localhost" />
+		<input type="text" name="db_database" value="" />
+		<input type="text" name="db_user" value="" />
+		<input type="password" name="db_password" value="" />
+		
+		<input type="submit" name="submit" value="Submit" />
+	
+	
+	</form>
+	
+</body>
+</html>
