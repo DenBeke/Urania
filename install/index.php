64e93431988e6a2322f7f02c4d1e44c1ea68cdc4
MathiasB <git@denbeke.be>
2014-02-09 14:31:44 +0100

    Error messages and style

--- install/index.php
+++ install/index.php
@@ -9,29 +9,38 @@ Date: February 2014
 
 require_once(dirname(__FILE__).'/install.php');
 
+$error;
 
 if(isset($_POST['db_host']) && isset($_POST['db_user']) && isset($_POST['db_password']) && isset($_POST['db_database'])) {
 
-	//Connect to database
-	$db_host = $_POST['db_host'];
-	$db_user = $_POST['db_user'];
-	$db_password = $_POST['db_password'];
-	$db_database = $_POST['db_database'];
+	try {
 	
-	$link = connectDatabase($db_host, $db_user, $db_password, $db_database);
+		//Connect to database
+		$db_host = $_POST['db_host'];
+		$db_user = $_POST['db_user'];
+		$db_password = $_POST['db_password'];
+		$db_database = $_POST['db_database'];
+		
+		$link = connectDatabase($db_host, $db_user, $db_password, $db_database);
+		
+		//Add the tables
+		createAlbumsTable($link);
+		createImagesTable($link);
+		
+		//Create the 'upload' dir
+		mkdir(dirname(__FILE__).'/../upload');
+		
+		
+		//TODO
+		//Fetch username
+		//Fetch the site url
+		//Write everything to the config file
 	
-	//Add the tables
-	createAlbumsTable($link);
-	createImagesTable($link)
+	}
 	
-	//Create the 'upload' dir
-	mkdir(dirname(__FILE__).'/../upload');
-	
-	
-	//TODO
-	//Fetch username
-	//Fetch the site url
-	//Write everything to the config file
+	catch (exception $e) {
+		$error = $e->getMessage();
+	}
 }
 
 ?>
@@ -44,8 +53,19 @@ if(isset($_POST['db_host']) && isset($_POST['db_user']) && isset($_POST['db_pass
 	<!--[if lt IE 9]>
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	
 </head>
 <body>
+
+	<?php 
+	if(isset($error)) {
+		echo '<div class="error">';
+		echo $error;
+		echo '</div>';
+	}
+	?>
 
 	<form method="post" action="index.php">
 	
